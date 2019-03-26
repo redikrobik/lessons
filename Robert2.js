ce202b8e4317cfb674b416b0395bd1d0788fc682
redikrobik <48002880+redikrobik@users.noreply.github.com>
2019-03-26 13:19:38 +0500

    Lesson 5 home

---
+++ Robert2.js
@@ -0,0 +1,18 @@
+			const Robert2 = ({ dop1, dop2 }) => {
+				return (
+					<React.Fragment>
+						<tr>
+							<td>1</td>
+							<td>Lesson {dop1}</td>
+						</tr>
+						<tr>
+							<td>2</td>
+							<td>Lesson</td>
+						</tr>
+						<tr>
+							<td>3</td>
+							<td>Lesson {dop2}</td>
+						</tr>
+					</React.Fragment>
+				)
+			}	
